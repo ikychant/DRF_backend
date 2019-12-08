aead54f4927d9bb0cebc657922155759304c2f92
ikychant <iki.informasi@gmail.com>
2019-12-08 12:55:37 +0700

    Bug in Django REST Swagger (Pending)

--- upwork/urls.py
+++ upwork/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework import routers
-
 from upwork.views import JobViewSet
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Jobs API')
 
 router = routers.DefaultRouter()
 router.register('job', JobViewSet)
-urlpatterns = []
+urlpatterns = [
+#     path('docs/', schema_view)
+]
 urlpatterns += router.urls
