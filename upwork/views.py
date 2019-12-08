094e31f2c9c7f050b5a27f4c05ae5b15889d597e
ikychant <iki.informasi@gmail.com>
2019-12-08 11:30:37 +0700

    Adding the JobViewSet

--- upwork/views.py
+++ upwork/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from upwork.models import Job
+from upwork.serializers import JobSerializer
+
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
